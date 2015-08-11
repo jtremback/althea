17c95202a887b1ab371db986f87944d3aa2c9249
Jehan <jehan.tremback@gmail.com>
2015-08-11 15:10:18 -0700

    simulation basically works

--- index.js
+++ index.js
@@ -15,78 +15,190 @@
 let nodes = {
   A: {
     peers: {
-      C: { pings: 100}
+      C: { cost: 0 }
+    },
+    routes: {
+      A: null,
+      B: 'C',
+      C: 'C',
+      D: 'C',
+      E: 'C',
+      F: 'C',
+      G: 'C',
+    },
+    pings: {
+      E: 3,
+      F: 7
     }
   },
   B: {
     peers: {
-      E: { pings: 40 },
-      D: { pings: 26 },
+      E: { cost: 0 },
+      D: { cost: 0 },
+    },
+    routes: {
+      A: 'D',
+      B: null,
+      C: 'D',
+      D: 'D',
+      E: 'E',
+      F: 'D',
+      G: 'E'
+    },
+    pings: {
+      A: 4,
+      G: 2
     }
   },
   C: {
     peers: {
-      A: { pings: 12 },
-      D: { pings: 23 },
+      A: { cost: 0 },
+      D: { cost: 0 },
+    },
+    routes: {
+      A: 'A',
+      B: 'D',
+      C: null,
+      D: 'D',
+      E: 'D',
+      F: 'D',
+      G: 'G'
+    },
+    pings: {
+      D: 8,
+      B: 4
     }
   },
   D: {
     peers: {
-      B: { pings: 1 },
-      C: { pings: 3 },
-      E: { pings: 39 },
-      F: { pings: 8 },
+      B: { cost: 0 },
+      C: { cost: 0 },
+      E: { cost: 0 },
+      F: { cost: 0 },
+    },
+    routes: {
+      A: 'C',
+      B: 'B',
+      C: 'C',
+      D: null,
+      E: 'E',
+      F: 'F',
+      G: 'F'
+    },
+    pings: {
+      C: 4,
+      G: 9
     }
   },
   E: {
     peers: {
-      B: { pings: 49 },
-      D: { pings: 32 },
-      G: { pings: 40 },
+      B: { cost: 0 },
+      D: { cost: 0 },
+      G: { cost: 0 },
+    },
+    routes: {
+      A: 'D',
+      B: 'B',
+      C: 'D',
+      D: 'D',
+      E: null,
+      F: 'D',
+      G: 'G'
+    },
+    pings: {
+      F: 5,
+      A: 12
     }
   },
   F: {
     peers: {
-      D: { pings: 33 },
-      E: { pings: 11 },
-      G: { pings: 22 },
+      D: { cost: 0 },
+      E: { cost: 0 },
+      G: { cost: 0 },
+    },
+    routes: {
+      A: 'D',
+      B: 'D',
+      C: 'D',
+      D: 'D',
+      E: 'G',
+      F: null,
+      G: 'G'
+    },
+    pings: {
+      B: 3,
+      D: 9
     }
   },
   G: {
     peers: {
-      E: { pings: 87 },
-      F: { pings: 1 },
+      E: { cost: 0 },
+      F: { cost: 0 },
+    },
+    routes: {
+      A: 'F',
+      B: 'E',
+      C: 'F',
+      D: 'F',
+      E: 'E',
+      F: 'F',
+      G: null
+    },
+    pings: {
+      B: 4,
+      C: 5
     }
   }
 }
 
 class Node {
-  constructor (id, nodes) {
+  constructor (id, opts) {
     this.id = id
-    this.peers = nodes[id].peers
-    this.nodes = nodes
+    this.peers = opts.peers
+    this.routes = opts.routes
+    this.pings = opts.pings
   }
 
-  recieve (message, senderId) {
-    console.log(`${this.id} recieved ${message} from ${senderId}`)
+  recieve (message, peerFrom) {
+    if (message.header.destinationAddress === this.id) {
+      console.log(this.id, 'recieved', message.body, 'from', message.header.sourceAddress, 'via', peerFrom)
+    } else {
+      // Forward along
+      this.send(message, peerFrom)
+    }
   }
 
-  send (message, recieverId) {
-    this.nodes[recieverId].recieve(message, this.id)
+  send (message, peerFrom) {
+    // Get peer for destinationAddress
+    let peerTo = this.routes[message.header.destinationAddress]
+
+    if (peerFrom) {
+      console.log('forwarding message from ' + peerFrom + ' to ' + peerTo)
+    }
+
+    // Send to peer
+    nodes[peerTo].recieve(message, this.id)
   }
 
   pingPeers () {
-    for (let key in this.peers) {
-      for (let i = 0; i < this.peers[key].pings; i++) {
-        this.send('ping', key)
+    for (let id in this.pings) {
+      for (let i = 0; i < this.pings[id]; i++) {
+        this.send({
+          header: {
+            destinationAddress: id,
+            sourceAddress: this.id
+          },
+          body: 'ping'
+        })
       }
     }
   }
 }
 
+
 function makeNodes (nodes) {
-  for (let key in nodes) {
-    nodes[key] = new Node(key, nodes)
+  for (let id in nodes) {
+    nodes[id] = new Node(id, nodes[id])
   }
 }
 
