89a7bf5d0e875865b584288fcf450ae891f25b05
Jehan Tremback <jehan@Jehans-MacBook-Pro.local>
2015-09-28 14:45:46 -0700

    bellman ford seems to work to some extent

--- bellman-ford/index.js
+++ bellman-ford/index.js
@@ -1,200 +1,156 @@
 // Allows us to use ES6 features like TCO
-require('babel/polyfill');
+// require('babel/polyfill');
 
 const verbose = true
-const helloInterval = 1000
+const updateInterval = 1000
 
 // let network = {
-//   A: { id: 'A', peers: ['C'], marginRate: 1, balance: 0 },
-//   B: { id: 'B', peers: ['D'], marginRate: 1, balance: 0 },
-//   C: { id: 'C', peers: ['A', 'D'], marginRate: 1, balance: 0 },
-//   D: { id: 'D', peers: ['B', 'C', 'E', 'F'], marginRate: 1, balance: 0 },
-//   E: { id: 'E', peers: ['D', 'F', 'G'], marginRate: 1, balance: 0 },
-//   F: { id: 'F', peers: ['D', 'E', 'G'], marginRate: 1, balance: 0 },
-//   G: { id: 'G', peers: ['E', 'F'], marginRate: 1, balance: 0 }
+//   A: { id: 'A', neighbors: ['C'] },
+//   B: { id: 'B', neighbors: ['D'] },
+//   C: { id: 'C', neighbors: ['A', 'D'] },
+//   D: { id: 'D', neighbors: ['B', 'C', 'E', 'F'] },
+//   E: { id: 'E', neighbors: ['D', 'F', 'G'] },
+//   F: { id: 'F', neighbors: ['D', 'E', 'G'] },
+//   G: { id: 'G', neighbors: ['E', 'F'] }
 // }
 
 let network = {
-  A: { id: 'A', peers: ['B'], marginRate: 1 },
-  B: { id: 'B', peers: ['A', 'C'], marginRate: 1 },
-  C: { id: 'C', peers: ['B', 'D'], marginRate: 1 },
-  D: { id: 'D', peers: ['C', 'E'], marginRate: 1 },
-  E: { id: 'E', peers: ['D', 'F'], marginRate: 1 },
-  F: { id: 'F', peers: ['E', 'G'], marginRate: 1 },
-  G: { id: 'G', peers: ['E'], marginRate: 1 }
+  S: { id: 'S', neighbors: ['A'], marginRate: 1 },
+  A: { id: 'A', neighbors: ['S', 'B'], marginRate: 1 },
+  B: { id: 'B', neighbors: ['A', 'C'], marginRate: 1 },
+  C: { id: 'C', neighbors: ['B', 'D'], marginRate: 1 },
+  D: { id: 'D', neighbors: ['C', 'E'], marginRate: 1 },
+  E: { id: 'E', neighbors: ['D', 'F'], marginRate: 1 },
+  F: { id: 'F', neighbors: ['E', 'G'], marginRate: 1 },
+  G: { id: 'G', neighbors: ['E'], marginRate: 1 }
 }
 
+// let network = {
+//   S: { id: 'S', neighbors: ['A'], marginRate: 1 },
+//   A: { id: 'A', neighbors: ['B', 'C'], marginRate: 1 },
+//   B: { id: 'B', neighbors: ['A', 'C'], marginRate: 1 },
+//   C: { id: 'C', neighbors: ['A', 'B'], marginRate: 1 }
+// }
 
 
 function initNodes (network) {
   for (let nodeId in network) {
     let node = network[nodeId]
+    // initialize route table with self
+    node.sources = {
+      // [node.id]: {
+      //   rate: 0,
+      //   nextHop: node.id
+      // }
+    }
 
-    // Add properties which are irrelevant to the network spec, but needed for
-    // network operation
-    node.receivable = {}
-    node.destinationRates = {}
-    node.totalMessagesForwarded = 0
+    node.helloSequence = 0
 
-    // Replace array of peer ids with object of actual peers
-    node.peers = node.peers.reduce((peerMap, peerId) => {
-      peerMap[peerId] = network[peerId]
-      return peerMap
+    // Replace array of neighbor ids with object of actual neighbors
+    node.neighbors = node.neighbors.reduce((neighborMap, neighborId) => {
+      neighborMap[neighborId] = network[neighborId]
+      return neighborMap
     }, {})
   }
 }
 
 
+// function flood (currentNode, message) {
+//   let { messageId, sourceId, destinationId } = message
 
-// function routingAlgorithm (node, destinationId) {
-//   return aStar({
-//     start: node,
-//     isEnd (node) { return node.id === destinationId },
-//     neighbor (node) {
-//       let peerArray = []
-//       for (let peerId in node.peers) {
-//         peerArray.push(node.peers[peerId])
-//       }
-//       return peerArray
-//     },
-//     distance () { return 1 },
-//     heuristic () { return 1 },
-//     hash (node) { return node.id }
-//   }).path[1]
+//   if (!currentNode.messagesReceived) {
+//     currentNode.messagesReceived = {}
+//   }
+
+//   if (currentNode.messagesReceived[messageId]) {
+//     return null
+//   }
+
+//   currentNode.messagesReceived[messageId] = true
+
+//   if (currentNode.id === destinationId) {
+//     console.log(`${currentNode.id} recieved ${messageId} from ${sourceId}`)
+//     return null
+//   }
+
+//   for (let peer of currentNode.peers) {
+//     console.log(`${currentNode.id} forwarded ${messageId}`)
+//     flood(peer, message)
+//   }
 // }
 
 
-
-function route (currentNode, upstreamNode, message) {
-  let { destinationId } = message
-
-  if (currentNode.id === destinationId) {
-    verbose && console.log(`${currentNode.id} received ` + JSON.stringify(message))
-    return null
-  }
-
-  let downstreamNode = routingAlgorithm(currentNode, destinationId)
-
-  verbose && console.log(`${currentNode.id} -> ${downstreamNode.id} ` + JSON.stringify(message))
-
-  if (upstreamNode) {
-    currentNode.totalMessagesForwarded = currentNode.totalMessagesForwarded + 1
-    updateOwed(currentNode, upstreamNode, message)
-  }
-
-  route(downstreamNode, currentNode, message)
-}
-
-
-
-function updateOwed (creditor, debtor, { destinationId }) {
-  // Create data structure holding amounts owed to creditor per debtor
-  creditor.receivable[debtor.id] = creditor.receivable[debtor.id] || {}
-  creditor.receivable[debtor.id][destinationId] = creditor.receivable[debtor.id][destinationId] || 0
-
-  // Increment number of unpaid messages
-  let numberOfMessages = creditor.receivable[debtor.id][destinationId] + 1
-
-  // Update amounts owed to creditor
-  creditor.receivable[debtor.id][destinationId] = numberOfMessages
-
-  // Make payment request if neccesary
-  if (numberOfMessages > 5) {
-    makePayment(
-      debtor,
-      creditor,
-      {
-        destinationId,
-        numberOfMessages,
-        messageRate: calculateMessageRate(creditor, destinationId)
-      }
+function update (currentNode, from, message) {
+  debugger
+  let { helloSequence, source, rate } = message
+  if (
+    // source is not in table
+    !currentNode.sources[source]
+    || // Or
+    (
+      // New rate is smaller
+      currentNode.sources[source].rate > rate
+      && // And
+      // helloSequence is bigger
+      currentNode.sources[source].helloSequence < helloSequence
     )
-  }
-}
+  ) {
+    console.log(`${currentNode.id} accepted update ${JSON.stringify(message)} from ${from.id}`)
+    currentNode.sources[source] = { helloSequence, rate, nextHop: from.id }
 
-
-
-function makePayment (payer, payee, paymentRequest) {
-  // Record cost of the destination for future reference
-  payer.destinationRates[paymentRequest.destinationId] = paymentRequest.messageRate
-
-  // Calculate txAmount and update balances
-  let txAmount = paymentRequest.numberOfMessages * paymentRequest.messageRate
-  payer.balance = payer.balance - txAmount
-  payee.balance = payee.balance + txAmount
-
-  verbose && console.log(`${payer.id} paid ${payee.id} ${txAmount}`)
-
-  payee.receivable[payer.id][paymentRequest.destinationId] = 0
-}
-
-
-
-function calculateMessageRate (creditor, destinationId) {
-  // If the destination is a peer, charge only the margin rate
-  if (creditor.peers[destinationId]) {
-    return creditor.marginRate
-  }
-
-  // If the destination rate is known, charge margin rate plus destination rate
-  if (creditor.destinationRates[destinationId]) {
-    return creditor.destinationRates[destinationId] + creditor.marginRate
-  }
-
-  // If the destination rate is unknown,
-  // charge margin rate plus unknown destination rate
-  return unknownDestinationRate + creditor.marginRate
-}
-
-
-
-// This closes out all receivable accounts
-function closeOut (network) {
-  for (let nodeId in network) {
-    let currentNode = network[nodeId]
-    let receivable = currentNode.receivable
-    if (receivable) {
-      for (let peerId in receivable) {
-        for (let destinationId in receivable[peerId]) {
-          makePayment(
-            currentNode.peers[peerId],
-            currentNode,
-            {
-              destinationId,
-              numberOfMessages: receivable[peerId][destinationId],
-              messageRate: calculateMessageRate(currentNode, destinationId)
-            }
-          )
-        }
+    for (let neighborId in currentNode.neighbors) {
+      let neighbor = currentNode.neighbors[neighborId]
+      console.log(`${currentNode.id} sent update ${JSON.stringify(message)} to ${neighbor.id}`)
+      let message = {
+        rate: rate + currentNode.marginRate,
+        helloSequence,
+        source
       }
+
+      update(neighbor, currentNode, message)
     }
+  } else {
+    console.log(`${currentNode.id} rejected update ${JSON.stringify(message)} from ${from.id}`)
   }
 }
 
+
+function hello (currentNode) {
+  currentNode.helloSequence = currentNode.helloSequence + 1
+  for (let neighborId in currentNode.neighbors) {
+    let neighbor = currentNode.neighbors[neighborId]
+    return update(neighbor, currentNode, {
+      rate: currentNode.marginRate,
+      helloSequence: currentNode.helloSequence,
+      source: currentNode.id
+    })
+  }
+}
 
 
 function decircularize () {
   for (let nodeId in network) {
     let node = network[nodeId]
-    let peerArray = []
-    for (let peerId in network[nodeId].peers) {
-      peerArray.push(peerId)
+    let neighborArray = []
+    for (let neighborId in network[nodeId].neighbors) {
+      neighborArray.push(neighborId)
     }
-    node.peers = peerArray
+    node.neighbors = neighborArray
   }
 }
 
 
-
 initNodes(network)
 
-for (var i = 0; i < 100; i++) {
-  route(network.A, null, {
-    messageId: i + '',
-    sourceId: 'A',
-    destinationId: 'G'
-  })
-}
+hello(network.S)
+
+// for (var i = 0; i < 100; i++) {
+//   route(network.A, null, {
+//     messageId: i + '',
+//     sourceId: 'A',
+//     sourceId: 'G'
+//   })
+// }
 
 
 // closeOut(network)
