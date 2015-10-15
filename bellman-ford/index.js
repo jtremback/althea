00c47d0405e22e93a247848e2e23e175f176474f
Jehan Tremback <jehan@Jehans-MacBook-Pro.local>
2015-10-15 13:24:10 -0700

    spurious starvation

--- bellman-ford/index.js
+++ bellman-ford/index.js
@@ -1,8 +1,3 @@
-// Allows us to use ES6 features like TCO
-// require('babel/polyfill');
-
-const verbose = true
-const updateInterval = 1000
 const ui = require('./ui.js')
 
 const tm = 1000
@@ -35,33 +30,8 @@ let infinityJSON = {
 
 
 
-//       C 1
-// 1 1 1/ \
-// S-A-B   E 1
-//      \ /
-//       D 1
-
-//       C 1
-// 1 ∞ 1/ \
-// S-A-B   E 1
-//      \ /
-//       D 1
-
-// let network = {
-//   S: { neighbors: ['A'], cost: 1 },
-//   A: { neighbors: ['S', 'B'], cost: 1 },
-//   B: { neighbors: ['A', 'C', 'D'], cost: 1 },
-//   C: { neighbors: ['B', 'E'], cost: 1 },
-//   D: { neighbors: ['B', 'E'], cost: 1 },
-//   E: { neighbors: ['C', 'D'], cost: 1 }
-// }
-
-// setTimeout(() => {
-//   network.L.cost = Infinity
-//   ui.updateNetwork(network)
-// }, 4 * tm)
-
-
+// Example from section 2.4
+// No routing loop should form after 4 seconds
 
 //       B
 //   1 1/|
@@ -75,101 +45,48 @@ let infinityJSON = {
 //     1\|
 //       C
 
-let network = {
-  nodes: {
-    S: {},
-    A: {},
-    B: {},
-    C: {}
-  },
-  edges: {
-    'S->A': { cost: 1 },
-    'A->S': { cost: 1 },
-
-    'B->A': { cost: 1 },
-    'A->B': { cost: 1 },
-
-    'B->C': { cost: 1 },
-    'C->B': { cost: 1 },
-
-    'C->A': { cost: 1 },
-    'A->C': { cost: 1 },
-  }
-}
-
-setTimeout(() => {
-  network.edges['S->A'].cost = Infinity
-  network.edges['A->S'].cost = Infinity
-  ui.updateNetwork(network)
-}, 4 * tm)
-
-
-
-//       B 1
-// 1 1 1/|
-// S-L-A |
-//      \|
-//       C 1
-
-//       B 1
-// 1 ∞ 1/|
-// S-L-A |
-//      \|
-//       C 1
-
 // let network = {
-//   S: { neighbors: ['L'], cost: 1 },
-//   L: { neighbors: ['S', 'A'], cost: 1 },
-//   A: { neighbors: ['L', 'B', 'C'], cost: 1 },
-//   B: { neighbors: ['A', 'C'], cost: 1 },
-//   C: { neighbors: ['A', 'B'], cost: 1 }
+//   nodes: {
+//     S: {},
+//     A: {},
+//     B: {},
+//     C: {}
+//   },
+//   edges: {
+//     'S->A': { cost: 1 },
+//     'A->S': { cost: 1 },
+
+//     'B->A': { cost: 1 },
+//     'A->B': { cost: 1 },
+
+//     'B->C': { cost: 1 },
+//     'C->B': { cost: 1 },
+
+//     'C->A': { cost: 1 },
+//     'A->C': { cost: 1 },
+//   }
 // }
 
 // setTimeout(() => {
-//   network.L.cost = Infinity
+//   network.edges['S->A'].cost = Infinity
+//   network.edges['A->S'].cost = Infinity
 //   ui.updateNetwork(network)
 // }, 4 * tm)
 
 
+// Can we select the better route?
 
-//      B 1
-// 1  1/|
-// S--A D 1
-//     \|
-//      C 1
+// //   A--B
+// // 1/  1 \1
+// // S      D
+// // 1\   5/
+// //   C--/
 
-//      B 1
-// ∞  1/|
-// S--A D 1
-//     \|
-//      C 1
-
-// let network = {
-//   S: { neighbors: ['A'], cost: 1 },
-//   A: { neighbors: ['S', 'B', 'C'], cost: 1 },
-//   B: { neighbors: ['A', 'D'], cost: 1 },
-//   C: { neighbors: ['A', 'D'], cost: 1 },
-//   D: { neighbors: ['B', 'C'], cost: 1 },
-// }
-
-// setTimeout(() => {
-//   network.S.cost = Infinity
-//   ui.updateNetwork(network)
-// }, 4 * tm)
-
-
-
-//   A--B
-// 1/  1 \1
-// S      D
-// 1\   5/
-//   C--/
-
-//   A--B
-// 1/  1 \1
-// S      D
-// 1\   1/
-//   C--/
+// //   A--B
+// // 1/  1 \1
+// // S      D
+// // 1\   1/
+// //   C--/
 
 // let network = {
 //   nodes: {
@@ -198,12 +115,15 @@ setTimeout(() => {
 // }
 
 // setTimeout(() => {
+//   console.log('changed cost of c-d')
 //   network.edges['C->D'].cost = 1
 //   network.edges['D->C'].cost = 1
 //   ui.updateNetwork(network)
 // }, 4 * tm)
 
 
+
+// Section 2.5: spurious starvation
 
 // 1 --A
 //  /  |
@@ -217,30 +137,30 @@ setTimeout(() => {
 //  \  |
 // 1 --B
 
-// let network = {
-//   nodes: {
-//     S: {},
-//     A: {},
-//     B: {},
-//     // C: {}
-//   },
-//   edges: {
-//     'S->A': { cost: 1 },
-//     'A->S': { cost: 1 },
+let network = {
+  nodes: {
+    S: {},
+    A: {},
+    B: {}
+  },
+  edges: {
+    'S->A': { cost: 1 },
+    'A->S': { cost: 1 },
 
-//     'B->A': { cost: 1 },
-//     'A->B': { cost: 1 },
+    'B->A': { cost: 1 },
+    'A->B': { cost: 1 },
 
-//     'S->B': { cost: 1 },
-//     'B->S': { cost: 1 },
+    'S->B': { cost: 2 },
+    'B->S': { cost: 2 },
+  }
+}
 
-//     // 'C->B': { cost: 1 },
-//     // 'B->C': { cost: 1 },
-
-//     // 'C->S': { cost: 1 },
-//     // 'S->C': { cost: 1 }
-//   }
-// }
+setTimeout(() => {
+  debugger
+  network.edges['S->A'].cost = Infinity
+  network.edges['A->S'].cost = Infinity
+  ui.updateNetwork(network)
+}, 4 * tm)
 
 
 
@@ -270,47 +190,61 @@ function initNodes (network) {
 function receiveUpdate (self, from, newSources) {
   newSources = infinityJSON.parse(newSources)
 
+  // Get cost of edge from network spec
+  // (Simulates hello and IHU from section 3.4.3)
   let edge = network.edges[self.id + '->' + from.id]
   let edgeCost = (edge && edge.cost) || 0
 
   for (let newSource of newSources) {
     let existingSource = self.sources[newSource.id]
 
+    // Don't accept updates for yourself
+    if (newSource.id === self.id) {
+      reject(newSource, existingSource.fd)
+
     // If a route for this destination does not yet exist, accept it.
-    if (!existingSource) {
+    } else if (!existingSource) {
+      console.log('!existingSource')
       accept(newSource)
 
-    // If the new route is from the same neighbor as the existing route, accept it.
-    } else if (existingSource.nextHop === from.id) {
-      if (newSource.cost + edgeCost < existingSource.cost) {
-        accept(newSource)
-      } else {
-        reject(newSource)
-      }
-
-    // If the new route has a lower cost than the existing route, accept it.
-    } else if (newSource.cost + edgeCost < existingSource.cost) {
-      accept(newSource)
+    // If the edge to the neighbor is lower cost than the smallest metric that
+    // we have ever advertised to our neighbors
+    // (D(B) < FD(A) from section 2.4)
+    } else if (newSource.cost < existingSource.fd) {
+      console.log('newSource.cost < existingSource.fd')
+      accept(newSource, existingSource.fd)
 
     } else {
-      reject(newSource)
+      reject(newSource, existingSource.fd)
     }
   }
 
-  function accept (newSource) {
-    self.sources[newSource.id] = { cost: newSource.cost + edgeCost, nextHop: from.id }
+  function accept (newSource, fd) {
+    if (!self.sources[newSource.id]) {
+      self.sources[newSource.id] = { cost: newSource.cost + edgeCost, nextHop: from.id, fd: Infinity }
+    } else {
+      self.sources[newSource.id].cost = newSource.cost + edgeCost
+      self.sources[newSource.id].nextHop = from.id
+    }
 
-    ui.log(`${self.id} accepted update ${infinityJSON.stringify(newSource)} from ${from.id}`)
+    ui.log(`${self.id} accepted update ${infinityJSON.stringify(newSource)} from ${from.id}, with fd ${fd}`)
     ui.updateNetwork(network)
   }
 
-  function reject (newSource) {
+  function reject (newSource, fd) {
     ui.log(`${self.id} rejected update ${infinityJSON.stringify(newSource)} from ${from.id}`)
   }
 }
 
-function transmit (callback) {
-  setTimeout(callback, Math.random() * 0.1 * tm)
+function transmit (neighbor, self, sources) {
+  // Check if edge cost is infinity
+  if (network.edges[self.id + '->' + neighbor.id].cost === Infinity) {
+    throw new Error()
+  }
+
+  setTimeout(() => {
+    receiveUpdate(neighbor, self, infinityJSON.stringify(sources))
+  }, Math.random() * 0.1 * tm)
 }
 
 function sendPeriodicUpdate (self) {
@@ -318,17 +252,23 @@ function sendPeriodicUpdate (self) {
 
   for (let sourceId in self.sources) {
     let source = self.sources[sourceId]
-    // let edge = network.edges[self.id + '->' + source.nextHop]
-    // let edgeCost = (edge && edge.cost) || 0
+
+    // Stores feasibility condition
+    // (FD(A) from section 2.4)
+    if (source.cost < source.fd) {
+      source.fd = source.cost
+    }
 
     sources.push({ id: sourceId, cost: source.cost })
   }
 
   for (let neighborId in self.neighbors) {
     let neighbor = self.neighbors[neighborId]
-    transmit(() => {
-      receiveUpdate(neighbor, self, infinityJSON.stringify(sources))
-    })
+    try {
+      transmit(neighbor, self, sources)
+    } catch (e) {
+      self.sources[neighborId].cost = Infinity
+    }
   }
 }
 
@@ -337,7 +277,7 @@ initNodes(network)
 ui.drawNetwork(network)
 
 for (let nodeId in network.nodes) {
-  let interval = setInterval(() => {
+  setInterval(() => {
     setTimeout(() => {
       sendPeriodicUpdate(network.nodes[nodeId])
     }, Math.random() * tm)
