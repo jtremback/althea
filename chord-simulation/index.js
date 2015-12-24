8434861f1cfd95ffc6329f9cb2cf7f1a1eb0760b
Jehan Tremback <jehan.tremback@gmail.com>
2015-12-24 12:39:11 -0800

    add chord simulation

---
+++ chord-simulation/index.js
@@ -0,0 +1,26 @@
+const jsHash = require('js-hash')
+
+let network = {
+  nodes: {
+    1: {
+      ipAddress: 1
+    },
+    2: {
+      ipAddress: 2
+    },
+    3: {
+      ipAddress: 3
+    },
+    4: {
+      ipAddress: 4
+    },
+    5: {
+      ipAddress: 5
+    },
+    6: {
+      ipAddress: 6
+    },
+  }
+}
+
+
